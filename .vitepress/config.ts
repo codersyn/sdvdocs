9986e9cfd88392b4e1cbd6e6ee8082d796ec64ca
codesyn <websyn@outlook.com>
2023-04-18 18:18:03 +0800

    博客源文件

---
+++ .vitepress/config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: '星露谷物语使用文档',
+  themeConfig: {
+    logo: '/logo.png',
+    siteTitle: '星露谷物语使用文档',
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/codersyn/sdvdocs' }
+    ]
+  }
+})
